b10c116a9744a4a6c43037387140fa5b72554143
Theron Boerner <hunterboerner@gmail.com>
2016-03-17 22:40:14 -0500

    Update rabbit-ci.sh

--- scripts/rabbit-ci.sh
+++ scripts/rabbit-ci.sh
@@ -17,6 +17,6 @@ if ! go get github.com/golang/tools/cmd/cover; then go get golang.org/x/tools/cm
 go get -d -v ./...
 
 export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+sudo sh -e /etc/init.d/xvfb start
 
 ./scripts/travis.sh
