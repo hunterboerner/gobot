3936c0e024a6fcddc0bdff6b8081a9928c9bf523
Theron Boerner <hunterboerner@gmail.com>
2016-03-17 21:35:27 -0500

    Update rabbit-ci.sh

--- scripts/rabbit-ci.sh
+++ scripts/rabbit-ci.sh
@@ -3,7 +3,7 @@ eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme 
 sudo add-apt-repository -y ppa:kubuntu-ppa/backports
 sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
 sudo apt-get update
-sudo apt-get install --force-yes libcv-dev libcvaux-dev libhighgui-dev libopencv-dev libsdl2-dev libsdl2-image-dev libsdl2 libusb-dev xvfb libgtk2.0-0
+sudo apt-get install -y libcv-dev libcvaux-dev libhighgui-dev libopencv-dev libsdl2-dev libsdl2-image-dev libsdl2 libusb-dev xvfb libgtk2.0-0
 go get github.com/axw/gocov/gocov
 go get github.com/mattn/goveralls
 if ! go get github.com/golang/tools/cmd/cover; then go get golang.org/x/tools/cmd/cover; fi
@@ -13,4 +13,4 @@ go get -d -v ./..
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-./travis.sh
+./scripts/travis.sh
