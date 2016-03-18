fcd57db5380ccd08189e994eeb2d04bdf6d559a4
Theron Boerner <hunterboerner@gmail.com>
2016-03-18 15:14:26 -0500

    Update rabbit-ci.sh

--- scripts/rabbit-ci.sh
+++ scripts/rabbit-ci.sh
@@ -5,16 +5,16 @@ cat <<EOF | sudo tee /etc/init.d/xvfb > /dev/null
 XVFB=/usr/bin/Xvfb
 XVFBARGS=":99 -ac -screen 0 1024x768x24"
 PIDFILE=/tmp/cucumber_xvfb_99.pid
-case "$1" in
+case "\$1" in
   start)
     echo -n "Starting virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
+    /sbin/start-stop-daemon --start --quiet --pidfile \$PIDFILE --make-pidfile --background --exec \$XVFB -- \$XVFBARGS
     echo "."
     ;;
   stop)
     echo -n "Stopping virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
-    rm -f $PIDFILE
+    /sbin/start-stop-daemon --stop --quiet --pidfile \$PIDFILE
+    rm -f \$PIDFILE
     echo "."
     ;;
   restart)
@@ -22,13 +22,13 @@ case "$1" in
     $0 start
     ;;
   *)
-  echo "Bad argument: $1"
-  echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
   exit 1
 esac
 exit 0
 EOF
+
 sudo chown root:root /etc/init.d/xvfb
+sudo chmod 0644 /etc/init.d/xvfb
 
 export GOPATH=$HOME/gopath
 export PATH=$HOME/gopath/bin:$PATH
